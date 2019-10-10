27e86623bd19d0ff1dd3c0f5ff6d6bdb7a8c9d92
Dazpel <alexxdav97@gmail.com>
2019-10-10 00:37:29 -0400

    New grid design

---
+++ reusable/navigation.js
@@ -0,0 +1,20 @@
+<div class="header__navigation">
+            <input type="checkbox" class="header__navigation__checkbox" id="navi-toggle">
+        
+
+            <label for="navi-toggle" class="header__navigation__button">
+                <span class="header__navigation__icon">&nbsp;</span>
+            </label>
+
+            <div class="header__navigation__background">&nbsp;</div>
+
+            <nav class="header__navigation__nav">
+                <ul class="header__navigation__list">
+                    <li class="header__navigation__item"><a href="#" class="header__navigation__link"><span>01</span>About Natous</a></li>
+                    <li class="header__navigation__item"><a href="#" class="header__navigation__link"><span>02</span>Your benfits</a></li>
+                    <li class="header__navigation__item"><a href="#" class="header__navigation__link"><span>03</span>Popular tours</a></li>
+                    <li class="header__navigation__item"><a href="#" class="header__navigation__link"><span>04</span>Stories</a></li>
+                    <li class="header__navigation__item"><a href="#" class="header__navigation__link"><span>05</span>Book now</a></li>
+                    </ul>
+            </nav>
+        </div>
